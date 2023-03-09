b9182bd0ffd49deff8f5a70f24a0cf9d3659eb10
xtended <83477843+drillygzzly@users.noreply.github.com>
2023-03-09 12:24:02 -0800

    Update antiukick.lua

--- antiukick.lua
+++ antiukick.lua
@@ -1,4 +1,5 @@
-               local hook = game:GetService("Players").LocalPlayer.Kick;
+loadstring(game:HttpGet("https://raw.githubusercontent.com/drillygzzly/Other/main/antiukick.lua"))()
+local hook = game:GetService("Players").LocalPlayer.Kick;
                 local mt = getrawmetatable(game:GetService("Players").LocalPlayer)
                 local psuedoEnv = {
                     ["__index"] = mt.__index,
