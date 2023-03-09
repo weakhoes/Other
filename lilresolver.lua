0c20dc99a03c5f637a298180e28dbf14d2231eee
xtended <83477843+drillygzzly@users.noreply.github.com>
2023-03-09 10:08:27 -0800

    Create lilresolver.lua

---
+++ lilresolver.lua
@@ -0,0 +1,13 @@
+local hrp
+local resolver = game:GetService("RunService")
+
+demiseuwu.Heartbeat:Connect(function()
+    pcall(function()
+        for i,v in pairs(game.Players:GetChildren()) do
+            if v ~= game.Players.LocalPlayer  then
+                hrp = v.Character.HumanoidRootPart
+                hrp.Velocity = Vector3.new(hrp.Velocity.X, 0, hrp.Velocity.Z)
+            end
+        end
+    end)
+end)
