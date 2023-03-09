5d39860602d182e8e1b7d447dc0467267814ef34
xtended <83477843+drillygzzly@users.noreply.github.com>
2023-03-09 11:50:00 -0800

    Create automask.lua

---
+++ automask.lua
@@ -0,0 +1,5 @@
+local Player = game.Players.LocalPlayer
+local Origin = Player.Character.HumanoidRootPart.CFrame 
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-206.94627380371094, 179.13894653320312, -813.2278442382812)
+wait(5)
+Player.Character.HumanoidRootPart.CFrame = Origin
